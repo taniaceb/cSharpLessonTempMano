46c0ee824d8743a13a9d1470d194b39ceeaa2a41
taniaceb <tatjana.cebatul@gmail.com>
2019-07-04 19:53:43 +0300

    new line

--- gitRemoteTest/gitRemoteTest/Program.cs
+++ gitRemoteTest/gitRemoteTest/Program.cs
@@ -7,6 +7,7 @@ namespace gitRemoteTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World 2!");
         }
     }
 }

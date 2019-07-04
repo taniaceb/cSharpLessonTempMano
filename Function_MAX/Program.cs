e23d779004ea7d798bda25704c311ac7cd52ecfc
taniaceb <tatjana.cebatul@gmail.com>
2019-07-04 20:19:20 +0300

    New function

---
+++ Function_MAX/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Function_MAX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Max(4, 8, 6));
+            Console.WriteLine(InRange(2, 5, 9));
+
+        }
+
+        static int Max(int a, int b, int c)
+        {
+            int m;
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > a && b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+
+        }
+        static bool InRange (int a, int b, int c)
+        {
+            bool d;
+            if ((a > b && a < c) || (a< b && a >c))
+            {
+                d= true;
+            }
+            else
+            {
+                d = false;
+            }
+            return d;
+        }
+    }
+}

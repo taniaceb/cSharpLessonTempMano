ebf803005b4dce91fde706fa716610ffc543acd8
taniaceb <tatjana.cebatul@gmail.com>
2019-07-04 19:38:51 +0300

    New files

---
+++ gitRemoteTest/gitRemoteTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitRemoteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

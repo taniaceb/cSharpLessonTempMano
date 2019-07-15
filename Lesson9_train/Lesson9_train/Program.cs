e9d388a95f06e40bdd0e261d12f5f1060ff97f40
taniaceb <tatjana.cebatul@gmail.com>
2019-07-15 17:14:41 +0300

    Praktika

---
+++ Lesson9_train/Lesson9_train/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson9_train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

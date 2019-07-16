da3cb9c4ffab51d704a64ec8a042e079051b63b8
taniaceb <tatjana.cebatul@gmail.com>
2019-07-16 16:00:49 +0300

    Change in program

---
+++ Lesson9_uzd/Lesson9_uzd_Child_Class/Lesson9_uzd_Child_Class/Nature/Bird.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson9_uzd_Child_Class.Nature
+{
+    class Bird
+    {
+        private int size;
+        protected string color;
+        public bool canFly;
+
+
+    }
+}
